0d454fec4dcd16e181eb2d67b7bd746a7413b355
bonzoholda <bhumibeka@gmail.com>
2025-11-08 10:02:18 +0700

    Add approve function to ERC20_ABI

--- dex_bot.py
+++ dex_bot.py
@@ -55,8 +55,21 @@ w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 # ---------- Minimal ABIs ----------
 ERC20_ABI = [
     {"constant": True, "inputs": [{"name": "_owner", "type": "address"}], "name": "balanceOf", "outputs": [{"name": "balance", "type": "uint256"}], "type": "function"},
-    {"constant": True, "inputs": [], "name": "decimals", "outputs": [{"name": "", "type": "uint8"}], "type": "function"}
+    {"constant": True, "inputs": [], "name": "decimals", "outputs": [{"name": "", "type": "uint8"}], "type": "function"},
+    {
+        "constant": False,
+        "inputs": [
+            {"name": "_spender", "type": "address"},
+            {"name": "_value", "type": "uint256"},
+        ],
+        "name": "approve",
+        "outputs": [{"name": "success", "type": "bool"}],
+        "payable": False,
+        "stateMutability": "nonpayable",
+        "type": "function",
+    }
 ]
+
 
 # QuickSwap/UniswapV2 Router minimal ABI
 ROUTER_ABI = [
